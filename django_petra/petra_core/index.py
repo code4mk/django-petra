86cb798e31b7ed35543aadd487e1fac8082ac28f
Mostafa Kamal <hiremostafa@gmail.com>
2024-11-07 01:50:13 +0600

    permission

--- django_petra/petra_core/index.py
+++ django_petra/petra_core/index.py
@@ -2,10 +2,12 @@ from rest_framework.viewsets import ViewSet as View
 from rest_framework.response import Response
 from rest_framework.serializers import ModelSerializer
 from django_petra.status import Status as MyStatus
+from django_petra.petra_core.permissions import Permissions
 
-class ViewSet(View):
+class ViewSet(View, Permissions):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        View.__init__(self, *args, **kwargs)
+        Permissions.__init__(self)
 
 class Response(Response):
     def __init__(self, *args, **kwargs):
